0d4220629aa3e5a42ac03c1f3344094e2a12b4a3
Suraj Chandrasekhar <surajchandrasekhar@Surajs-MacBook-Air.local>
2017-10-29 21:15:24 -0700

    added code for PC study

---
+++ pcmac.R
@@ -0,0 +1,11 @@
+computers <- read.csv("pilotStudy.csv")
+library(ggplot2)
+library(dplyr)
+ggplot(data=computers) + geom_bar(mapping=aes(x=What.operating.system.do.you.primarily.use.)) + ggtitle("What Operating System Do You Use?")
+pc.data <- computers %>% filter(What.operating.system.do.you.primarily.use.=="PC")
+pc.gpa. <- ggplot(data=pc.data) + geom_bar(mapping=aes(x=What.is.your.GPA.)) + ggtitle("What is your average GPA if you have a PC?")
+pc.gpa.
+mac.data <- computers %>% filter(What.operating.system.do.you.primarily.use.=="MAC")
+mac.gpa. <- ggplot(data=mac.data) + geom_bar(mapping=aes(x=What.is.your.GPA.)) + ggtitle("What is your average GPA if you have a PC?")
+mac.gpa.
+
