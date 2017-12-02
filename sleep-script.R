f86b187ffb44b93324dfdb74babc74ac292d1ddf
leepolla <leepolla@gmail.com>
2017-12-02 15:00:58 -0800

    did linear regression analysis

--- sleep-script.R
+++ sleep-script.R
@@ -9,12 +9,17 @@ if(!require(DEoptimR)){install.packages("DEoptimR"); require(DEoptimR)}
 
 if(!require(caret)){install.packages("caret"); require(caret)} # ML package WITHOUT its dependencies. Should not take as long
 if(!require(dplyr)){install.packages("dplyr"); require(dplyr)}
+if(!require(broom)){install.packages("broom"); require(broom)}
 set.seed(370)
 
 if(!require(caret)){install.packages("caret", dependencies = c("Depends", "Suggests")); require(caret)}
 
+
+
 # load data
 sleep_data <- read.csv("./Sleep Drinking - Sheet1.csv")
+
+sleep_data <- sleep_data %>% dplyr::select(-LarkOwl, -DepressionStatus, -AnxietyStatus, -Stress)
 
 # Scatter plot
 
@@ -30,3 +35,73 @@ model <- train(GPA ~., data=sleep_data, method = "knn", trControl = control)
 importance <- varImp(model)
 
 ggplot(importance)
+
+linear_model <- lm (GPA ~ Drinks, data = sleep_data)
+
+parameters <- tidy(linear_model)$estimate
+
+sleep_data_small <- sleep_data %>%
+  #mutate(GPA_negation = 4.0 - sleep_data$GPA) %>%
+  dplyr::select(Drinks, GPA) %>%
+  mutate(linear_model_prediction = parameters[1] + parameters[2] * sleep_data$Drinks)
+
+#head(sleep_data)
+
+ggplot(sleep_data_small , aes(Drinks)) +
+  geom_point(aes(y= GPA), size = 1) +
+  geom_line(aes(y= linear_model_prediction), size = 1, colour = "grey30")
+
+model_linear_function <- function(a, data) {
+  a[1] + data$Drinks * a[2]
+}
+
+model_function = model_linear_function
+measure_distance <- function(mod_params, data) {
+  diff <- data$GPA - model_function(mod_params, data)
+  sqrt(mean(diff ^ 2))
+}
+
+# Here's a line fit with the objective function that lm uses - squares of the residuals
+best <- optim(c(3.4, 0), measure_distance, data = sleep_data_small)
+
+
+# values for the coefficients
+parameters
+best$par
+
+measure_distance_mad <- function(mod_params, data) {
+  diff <- data$GPA - model_function(mod_params, data)
+  mean(abs(diff))
+}
+
+# make use of a different distance function here (such as mean absolute)
+mad_fit <- optim(c(3.4, 0), measure_distance_mad, data = sleep_data_small)
+mad_fit$par
+
+sleep_data_small <- sleep_data_small %>%
+  mutate(lm_mad = mad_fit$par[1] + mad_fit$par[2] * sleep_data$Drinks)
+
+sleep_data_small <- sleep_data_small %>% 
+  mutate(
+    residuals_mad = GPA - lm_mad,
+    residuals = GPA - linear_model_prediction
+  )
+
+
+# plot the residuals to compare them
+ggplot(sleep_data_small, aes(Drinks)) +
+  geom_point(aes(y=residuals)) + 
+  geom_point(aes(y=residuals_mad), color = "red") +
+  geom_abline(intercept=0, slope = 0)
+
+summary(linear_model)
+
+
+
+
+
+
+
+
+
+
